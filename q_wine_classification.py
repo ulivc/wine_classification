ccb65c01380fbaecc5a7672dd8759a007296183b
ulivc <ulivc@student.kit.edu>
2022-11-08 12:34:36 +0100

    running with little accuracy

--- q_wine_classification.py
+++ q_wine_classification.py
@@ -7,49 +7,50 @@ from qiskit import BasicAer, execute
 from qiskit.algorithms.optimizers import SPSA
 from qiskit_machine_learning.algorithms.classifiers import VQC
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
-# 1. prepare data
-#TRAIN_DATA, TRAIN_LABEL, TEST_DATA, TEST_LABEL = wine(training_size=140, test_size=30, n=13)
-
-
+# configuration
+feature_size = 2
+training_size = 5
 algorithm_globals.random_seed = 3142
 np.random.seed(algorithm_globals.random_seed)
 
-TRAIN_DATA, TRAIN_LABELS, TEST_DATA, TEST_LABELS = wine(
-    training_size=20, test_size=5, n=13, one_hot=False
+# prepare data
+TRAIN_DATA, train_labels_oh, TEST_DATA, test_labels_oh = wine(
+    training_size=training_size, test_size=5, n=feature_size, one_hot=True
 )
-
-# data encoding
-FEATURE_MAP = ZZFeatureMap(feature_dimension=13, reps=2)
-# variational circuit
-VAR_FORM = TwoLocal(13, ["ry", "rz"], "cz", reps=2)
-
-# fügt die feature_map mit dem variational circuit zusammen
-AD_HOC_CIRCUIT = FEATURE_MAP.compose(VAR_FORM)
-AD_HOC_CIRCUIT.measure_all()
-
-encoder = OneHotEncoder()
+""" encoder = OneHotEncoder()
 train_labels_oh = encoder.fit_transform(TRAIN_LABELS.reshape(-1, 1)).toarray()
 test_labels_oh = encoder.fit_transform(TEST_LABELS.reshape(-1,1)).toarray()
-
-initial_point = np.random.random(VAR_FORM.num_parameters)
-""" initial_point = np.array([0.3200227 , 0.6503638 , 0.55995053,
-                          0.96566328, 0.38243769, 0.90403094,
-                          0.82271449, 0.26810137, 0.61076489,
-                          0.82301609, 0.11789148, 0.29667125]) """
+ """
+# store results for plotting
 class OptimizerLog:
     """Log to store optimizer's intermediate results"""
     def __init__(self):
         self.evaluations = []
         self.parameters = []
         self.costs = []
+        self.count = 1
+        print("hallo")
     def update(self, evaluation, parameter, cost, _stepsize, _accept):
         """Save intermediate results. Optimizer passes five values but we ignore the last two."""
         self.evaluations.append(evaluation)
         self.parameters.append(parameter)
         self.costs.append(cost)
+        self.count += 1
+        print(self.count)
     
+
+# prepare circuit components
+# data encoding
+FEATURE_MAP = ZZFeatureMap(feature_dimension=feature_size, reps=2)
+# variational circuit
+VAR_FORM = TwoLocal(feature_size, ["ry", "rz"], "cz", reps=2)
+
+initial_point = np.random.random(VAR_FORM.num_parameters)
 log = OptimizerLog()
+
+# initialize circuit
 vqc = VQC(feature_map=FEATURE_MAP,
           ansatz=VAR_FORM,
           loss='cross_entropy',
@@ -57,15 +58,66 @@ vqc = VQC(feature_map=FEATURE_MAP,
           initial_point=initial_point,
           quantum_instance=BasicAer.get_backend('qasm_simulator'))
 
-# training?
+# training (100 evaluations, why?)
 vqc.fit(TRAIN_DATA, train_labels_oh)
 
+# calculate accuracy
+accuracy = vqc.score(TEST_DATA, test_labels_oh)
+print(accuracy)
+
+# plot loss 
 fig = plt.figure()
 plt.plot(log.evaluations, log.costs)
 plt.xlabel('Steps')
 plt.ylabel('Cost')
+plt.title(f"{accuracy}_{feature_size}_wine_classification")
 print(plt.show())
-# 1.1 one hot encode data
-# create logger
-# 2. create circuit
-# 3. plot circuit
+fig.savefig(f"plots/qcosts_{accuracy}_{feature_size}_{training_size}.png")
+
+
+fig1 = plt.figure(figsize=(9, 6))
+
+for feature, label in zip(TRAIN_DATA, train_labels_oh):
+    COLOR = "C1" if label[0] == 0 else "C0"
+    plt.scatter(feature[0], feature[1], marker="o", s=100, color=COLOR)
+
+for feature, label, pred in zip(TEST_DATA, test_labels_oh, vqc.predict(TEST_DATA)):
+    COLOR = "C1" if pred[0] == 0 else "C0"
+    plt.scatter(feature[0], feature[1], marker="s", s=100, color=COLOR)
+    if not np.array_equal(label, pred):  # mark wrongly classified
+        plt.scatter(
+            feature[0],
+            feature[1],
+            marker="o",
+            s=500,
+            linewidths=2.5,
+            facecolor="none",
+            edgecolor="C3",
+        )
+
+legend_elements = [
+    Line2D([0], [0], marker="o", c="w", mfc="C1", label="A", ms=10),
+    Line2D([0], [0], marker="o", c="w", mfc="C0", label="B", ms=10),
+    Line2D([0], [0], marker="s", c="w", mfc="C1", label="predict A", ms=10),
+    Line2D([0], [0], marker="s", c="w", mfc="C0", label="predict B", ms=10),
+    Line2D(
+        [0],
+        [0],
+        marker="o",
+        c="w",
+        mfc="none",
+        mec="C3",
+        label="wrongly classified",
+        mew=2,
+        ms=15,
+    ),
+]
+
+plt.legend(handles=legend_elements, bbox_to_anchor=(1, 1), loc="upper left")
+
+plt.title("Training & Test Data")
+plt.xlabel("x")
+plt.ylabel("y")
+print(plt.show())
+fig1.savefig(f"plots/qresults_{accuracy}_{feature_size}_{training_size}.png")
+
