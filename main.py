change in probability computation, fixed mistake

main.py
@@ -5,9 +5,9 @@ import numpy as np
 
 # configuration
 feature_size = 3  # min 3
-training_size = 10
+training_size = 100
 test_size = 20
-maxiter = 1
+maxiter = 10
 seed = 3142
 
 
@@ -33,4 +33,5 @@ np.savetxt(
 )
 
 # load model
+# y muss in model.training als initial_point übergeben werden oder als opt_parameters bei model.test_classifier(opt_parameters)
 y = np.loadtxt(f"trained_models/opt_var_{accuracy}_{feature_size}_{training_size}.txt")

q_full_wine_classification.py
@@ -14,8 +14,8 @@ from sklearn.utils import shuffle
 # configuration
 # kleinste feature_size ist 3
 feature_size = 3
-training_size = 120
-maxiter = 200
+training_size = 100
+maxiter = 100
 
 algorithm_globals.random_seed = 3142
 np.random.seed(algorithm_globals.random_seed)
@@ -26,7 +26,6 @@ TRAIN_DATA, train_labels_oh, TEST_DATA, test_labels_oh = wine(
 )
 # shuffle dataset
 TRAIN_DATA, train_labels_oh = shuffle(TRAIN_DATA, train_labels_oh)
-TEST_DATA, test_labels_oh = shuffle(TEST_DATA, test_labels_oh)
 # prepare circuit components
 # data encoding
 FEATURE_MAP = ZZFeatureMap(feature_dimension=feature_size, reps=3)
@@ -77,13 +76,20 @@ def label_probability(results):
         if bitstring[5] == "1":
             probabilities[0] += counts / shots
     return probabilities """
+    number_ones = 0
     for bitstring, counts in results.items():
         if bitstring[0] == "1":
-            probabilities[2] += counts / shots
+            number_ones += counts
+            probabilities[2] += counts 
         if bitstring[1] == "1":
-            probabilities[1] += counts / shots
+            number_ones += counts
+            probabilities[1] += counts 
         if bitstring[2] == "1":
-            probabilities[0] += counts / shots
+            number_ones += counts
+            probabilities[0] += counts 
+    probabilities[0] = probabilities[0]/ number_ones
+    probabilities[1] = probabilities[1]/ number_ones
+    probabilities[2] = probabilities[2]/ number_ones
     return probabilities  
     """ for bitstring, counts in results.items():
         if bitstring == "100":
@@ -170,7 +176,7 @@ def objective_function(variational):
     return cost_function(TRAIN_DATA, train_labels_oh, variational)
 
 
-result = optimizer.minimize(objective_function, y)
+result = optimizer.minimize(objective_function, initial_point)
 
 opt_var = result.x
 opt_value = result.fun

wine_model.py
@@ -20,19 +20,19 @@ class Model:
             training_size=self.training_size,
             test_size=self.test_size,
             n=self.feature_size,
-            one_hot=False,
+            one_hot=False, 
         )
         self.TRAIN_DATA, self.TRAIN_LABELS = shuffle(self.TRAIN_DATA, self.TRAIN_LABELS)
-        self.TEST_DATA, self.TEST_LABELS = shuffle(self.TEST_DATA, self.TEST_LABELS)
+        
 
         return self.TRAIN_DATA, self.TRAIN_LABELS, self.TEST_DATA, self.TEST_LABELS
 
     def prepare_circuit_structure(self):
         # prepare circuit components
         # data encoding
-        self.FEATURE_MAP = ZZFeatureMap(feature_dimension=self.feature_size, reps=3)
+        self.FEATURE_MAP = ZZFeatureMap(feature_dimension=self.feature_size, reps=5)
         # variational circuit
-        self.VAR_FORM = TwoLocal(self.feature_size, ["ry", "rz"], "cz", reps=3)
+        self.VAR_FORM = TwoLocal(self.feature_size, ["ry", "rz"], "cz", reps=5)
 
         # fügt die feature_map mit dem variational circuit zusammen
         self.WINE_CIRCUIT = self.FEATURE_MAP.compose(self.VAR_FORM)
@@ -60,15 +60,21 @@ class Model:
         """Converts a dict of bitstrings and their counts to parities and their counts"""
         shots = sum(results.values())
         probabilities = {0: 0.0, 1: 0.0, 2: 0.0}
+        number_ones = 0
         for bitstring, counts in results.items():
             if bitstring[0] == "1":
-                probabilities[2] += counts / shots
+                number_ones += counts
+                probabilities[2] += counts 
             if bitstring[1] == "1":
-                probabilities[1] += counts / shots
+                number_ones += counts
+                probabilities[1] += counts 
             if bitstring[2] == "1":
-                probabilities[0] += counts / shots
-        return probabilities
-
+                number_ones += counts
+                probabilities[0] += counts 
+        probabilities[0] = probabilities[0]/ number_ones
+        probabilities[1] = probabilities[1]/ number_ones
+        probabilities[2] = probabilities[2]/ number_ones
+        return probabilities  
         # using 6 qubits, currently not helping
         """ for bitstring, counts in results.items():
         if bitstring[0] == "1":
@@ -121,12 +127,13 @@ class Model:
     def cross_entropy_loss(self, classification, expected):
         """Calculate accuracy of predictions using cross entropy loss."""
         p = classification.get(expected)
+        print(p)
         return -np.log(p + 1e-10)
 
     # cost function
     def cost_function(self, variational):
         """Evaluates performance of our circuit with variational parameters on data"""
-        classifications = self.classification_probability(variational, self.TEST_DATA)
+        classifications = self.classification_probability(variational=variational, data=self.TRAIN_DATA)
         cost = 0
         for i, classification in enumerate(classifications):
             cost += self.cross_entropy_loss(classification, self.TRAIN_LABELS[i])